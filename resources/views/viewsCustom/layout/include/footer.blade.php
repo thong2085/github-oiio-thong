6b2cbd66280f616fa03f1dce437d7a3bc9944d7d
HoangVan6655 <89716077+HoangVan6655@users.noreply.github.com>
2023-07-06 15:06:42 +0700

    UI Update

--- resources/views/viewsCustom/layout/include/footer.blade.php
+++ resources/views/viewsCustom/layout/include/footer.blade.php
@@ -1,10 +1,12 @@
 <div class="container-fluid d-flex justify-content-between">
+    {{-- Chính sách --}}
     <span class="text-muted col-8 d-block text-center text-sm-start d-sm-inline-block" style="text-align: left !important;">
         <a href="#" style="color: #A6A4A4; display: inline-flex; align-items: center; gap: 12px; margin-right: 12px">Chính sách quyền riêng tư</a>
         <a href="#" style="color: #A6A4A4; display: inline-flex; align-items: center; gap: 12px; margin-right: 12px">Điều khoản và điều kiện giao dịch</a>
         <a href="#" style="color: #A6A4A4; display: inline-flex; align-items: center; gap: 12px;">Chính sách thanh toán</a>
     </span>
 
+    {{-- Logo --}}
     <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"
           style="color: #A6A4A4; display: inline-flex; align-items: flex-start; gap: 15px;">
         <a href="https://www.facebook.com/groups/shopee.tittok.facebcok">
