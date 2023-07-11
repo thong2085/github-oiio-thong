c719126ec5eebe9d0645e12578ebb46be28ebc1a
HoangVan6655 <89716077+HoangVan6655@users.noreply.github.com>
2023-07-11 09:50:36 +0700

    Privacy Create-Edit-Delete

---
+++ database/migrations/2023_07_11_094153_create_privacy_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('privacies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('admin_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('avatar')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('privacies');
+    }
+};
